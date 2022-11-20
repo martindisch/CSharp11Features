639a3d748af406818ae69e4a719b0a6357057add
Martin Disch <martindisch@gmail.com>
2022-11-20 22:24:01 +0100

    Add palindrome check as list pattern example

---
+++ ListPattern/Program.cs
@@ -0,0 +1,17 @@
+// Finally this is as elegant as its Rust counterpart
+// https://adventures.michaelfbryan.com/posts/daily/slice-patterns/#checking-for-palindromes
+
+bool IsPalindrome(char[] characters)
+{
+    return characters switch
+    {
+        [var first, .. var middle, var last] => first == last && IsPalindrome(middle),
+        [] or [_] => true,
+    };
+}
+
+var palindrome = "abba";
+var notPalindrome = "abc";
+
+Console.WriteLine($"{palindrome}: {IsPalindrome(palindrome.ToCharArray())}");
+Console.WriteLine($"{notPalindrome}: {IsPalindrome(notPalindrome.ToCharArray())}");
