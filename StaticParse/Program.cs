3bcaa4d336e037cd4eb56b2c5f3b645a45f3147c
Martin Disch <martindisch@gmail.com>
2022-11-20 22:26:17 +0100

    Inline function for generic parsing

--- StaticParse/Program.cs
+++ StaticParse/Program.cs
@@ -4,7 +4,12 @@ var parsed = PoorCulture.Parse(stringRepresentation, null);
 
 Console.WriteLine($"Original: {original}, Parsed: {parsed}");
 
-var genericallyParsedInt = Util.GenericParse<int>("5");
-var genericallyParsedPoorCulture = Util.GenericParse<PoorCulture>("de-CH");
+static T GenericParse<T>(string input) where T : IParsable<T>
+{
+    return T.Parse(input, null);
+}
+
+var genericallyParsedInt = GenericParse<int>("5");
+var genericallyParsedPoorCulture = GenericParse<PoorCulture>("de-CH");
 
 Console.WriteLine($"Int: {genericallyParsedInt}, PoorCulture: {genericallyParsedPoorCulture}");
--- StaticParse/Util.cs
+++
@@ -1,7 +0,0 @@
-public static class Util
-{
-    public static T GenericParse<T>(string input) where T : IParsable<T>
-    {
-        return T.Parse(input, null);
-    }
-}
